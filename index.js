bd4800cf1eb380feb47c8916b5fcf252e326821a
Alberto Miranda <codealchemist@gmail.com>
2020-04-20 01:06:32 -0300

    First commit.

---
+++ index.js
@@ -0,0 +1,19 @@
+const Turn = require('node-turn')
+
+const PORT = process.env.PORT || 3478
+const USER = process.env.USER || 'test'
+const PASS = process.env.PASS || 'test'
+
+const server = new Turn({
+  // set options
+  authMech: 'long-term',
+  credentials: {
+    [USER]: PASS
+  },
+  listeningPort: PORT
+})
+
+server.start()
+console.log(new Date().toString())
+console.log('Holis TURN server started!')
+console.log('-'.repeat(80))
